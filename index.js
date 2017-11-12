5f963867d15b74cc61da226d92a00174818d14d5
AnthonyCatalfo <anthonycatalfo@gmail.com>
2017-11-12 12:13:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+console.log(`${greeting}, ${firstName}!`)
+function logShout(string) {
+  console.log(`${string.toUpperCase()}`)
+}
+function logWhisper(string) {
+  console.log(`${string.toLowerCase()}`)
+}
+function sayHiToGrandma(string) {
+  if (string===string.toLowerCase()){
+
+
+  }
+
+
+  console.log(`${string.toUpperCase()}`)
+
+}
